452701abc9e461ae84a38cf7882dc909daed386d
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-12 18:48:43 -0300

    add env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  API_BASE: 'https://demo.dotcms.com',
+  API_BASE_QUERY_URL:
+    'https://demo.dotcms.com/api/content/render/false/query/+contentType:Blog%20DATE_QUERY/orderby/modDate%20desc/limit/LIMIT/offset/OFFSET',
+  API_BASE_ID_URL: 'https://demo.dotcms.com/api/content/id/IDENTIFIER',
+};
