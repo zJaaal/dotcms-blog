acb8e7652cb6a7af597a109556d716c884dacf8d
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-12 19:53:10 -0300

    add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,7 +15,7 @@ import { NewsComponent } from './components/news/news.component';
     HeaderComponent,
     NewsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
