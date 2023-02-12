98e47ceec9016c8cb55cf7fe98dd80f8e379537a
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-12 18:20:21 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+    HeaderComponent,
+    NewsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
