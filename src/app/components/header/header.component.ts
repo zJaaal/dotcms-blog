a308bb23b928c0382d1ffabf82764c27c808a314
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-13 14:56:40 -0300

    add handling of year change

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { YearService } from 'src/app/services/year/year.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  years: string[] = ['All', '2019', '2020', '2021'];
+  constructor(private yearService: YearService) {}
 
+  handleYearChange({ target }: Event) {
+    this.yearService.setCurrentYear((target as HTMLSelectElement).value);
+  }
 }
