b3a24cb6b2a8e2e99abf3b2c99c62d45e644b0ed
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-13 19:50:09 -0300

    create dot-image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { BlogItemComponent } from './components/blog-list/components/blog-item/blog-item.component';
+import { DotImageComponent } from './components/dot-image/dot-image.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { BlogComponent } from './components/blog/blog.component';
     BlogListComponent,
     HeaderComponent,
     BlogComponent,
+    BlogItemComponent,
+    DotImageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
