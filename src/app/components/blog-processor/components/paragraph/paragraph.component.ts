6dd66eed401d27bd74c53e0626ed4cce827f429f
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-15 14:36:28 -0300

    fix styles

--- src/app/components/blog-processor/components/paragraph/paragraph.component.ts
+++ src/app/components/blog-processor/components/paragraph/paragraph.component.ts
@@ -2,9 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-paragraph',
-  template: `<p [ngStyle]="attrs">
-    <ng-content></ng-content>
-  </p>`,
+  template: `<div class="mb-2 mt-2">
+    <p [ngStyle]="attrs">
+      <ng-content></ng-content>
+    </p>
+  </div>`,
   styleUrls: ['./paragraph.component.css'],
 })
 export class ParagraphComponent {
