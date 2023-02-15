b37a0954067f6625e3dd5ebac73e60568e0bf0f4
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-15 14:31:26 -0300

    add heading component

---
+++ src/app/components/blog-processor/components/heading/heading.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.css'],
+})
+export class HeadingComponent {
+  @Input() attrs: any = {};
+}
